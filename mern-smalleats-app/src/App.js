389b6910b208f89243c08f955d791dd47bc4a076
Mitchell McKenna <mitchelljackmck@gmail.com>
2021-03-29 12:22:27 -0400

    todolist template done

--- mern-smalleats-app/src/App.js
+++ mern-smalleats-app/src/App.js
@@ -1,13 +1,44 @@
-//import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
-function App() {
-  return (
-    <div className="container">
-      <h2>SMALLEATS - APP</h2>
-    </div>
-  );
+import logo from "./SMALL-EATS-LLC-ALT-TRANSPsnip.png";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="250" height="75" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
